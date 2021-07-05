a9e6dc1b862656e9e3591cce43194e5f63c4a92d
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-07-05 14:07:10 -0300

    Refatoração resource/CategoriaResource

--- src/main/java/com/pedro/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/pedro/cursomc/resources/CategoriaResource.java
@@ -1,5 +1,11 @@
 package com.pedro.cursomc.resources;
 
+import com.pedro.cursomc.domain.Categoria;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
     @GetMapping
-    public String listar() {
-        return "REST está funcionando!";
+    public List<Categoria> listar() {
+        
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.addAll(Arrays.asList(cat1, cat2));
+
+        return categorias;
     }
 }
