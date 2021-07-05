12abb92d5fcaf9a91441705e9f2f614575c0f873
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-07-05 11:47:43 -0300

    REST testado

--- src/main/java/com/pedro/cursomc/CursomcApplication.java
+++ src/main/java/com/pedro/cursomc/CursomcApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication {
-
-	//Aprendendo a trabalhar com branch's
-
 	public static void main(String[] args) {
-		String merge = "teste";
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
